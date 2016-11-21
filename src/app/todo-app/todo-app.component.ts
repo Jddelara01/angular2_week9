80d5f0c38c739cf63db9352a347ce7130d13ef7b
John De Lara <jddelara01@gmail.com>
2016-11-21 14:11:47 +0000

    first commit

---
+++ src/app/todo-app/todo-app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-todo-app',
+  templateUrl: 'todo-app.component.html',
+  styleUrls: ['todo-app.component.css']
+})
+export class TodoAppComponent {
+
+  constructor() {}
+
+}
+
+/*import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-app',
+  templateUrl: './todo-app.component.html',
+  styleUrls: ['./todo-app.component.css']
+})
+export class TodoAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}*/
