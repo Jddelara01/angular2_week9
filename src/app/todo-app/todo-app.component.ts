ddba79b83b0f8a90a96e28495cb191909a219454
John De Lara <jddelara01@gmail.com>
2016-11-21 14:31:49 +0000

    second commit

--- src/app/todo-app/todo-app.component.ts
+++ src/app/todo-app/todo-app.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Todo} from '../todo';
+import {TodoService} from '../todo.service';
 
 @Component({
   moduleId: module.id,
-  selector: 'app-todo-app',
+  selector: 'todo-app',
   templateUrl: 'todo-app.component.html',
-  styleUrls: ['todo-app.component.css']
+  styleUrls: ['todo-app.component.css'],
+  providers: [TodoService]
 })
 export class TodoAppComponent {
 
-  constructor() {}
+  newTodo: Todo = new Todo();
+
+  constructor(private todoService: TodoService) {
+  }
+
+  addTodo() {
+    this.todoService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoService.getAllTodos();
+  }
 
 }
 
